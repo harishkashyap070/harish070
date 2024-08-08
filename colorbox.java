30ec9aa4941d8b18a300fec7c58c601f79e19467
harishkashyap070 <harishok04@gmail.com>
2024-08-08 13:44:05 +0530

    java
    java code

---
+++ colorbox.java
@@ -0,0 +1,36 @@
+public class colorbox {      //static 
+    static public int length;
+    public int width;
+
+    static public void show()
+    {
+     System.out.println("welcome you");
+     System.out.println(" in programming woirld");
+     
+
+    }
+    
+
+
+}
+class box {
+
+public static void main(String[] args) {
+  
+ /*  colorbox c1=new colorbox();
+    colorbox c2=new colorbox();
+
+    
+    c1.length=55;
+    c2.width=65;
+    System.out.println(c2.length);
+    System.out.println(c1.width);*/
+   
+
+    colorbox.show();
+    System.out.println(colorbox.length);
+
+    
+}
+
+}
